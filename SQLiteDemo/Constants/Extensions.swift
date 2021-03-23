e8a21631469d17cc43803963706031e4a0c7b716
nad27298 <70792702+nad27298@users.noreply.github.com>
2021-03-23 14:53:57 +0700

    Update Extensions.swift
    add

--- SQLiteDemo/Constants/Extensions.swift
+++ SQLiteDemo/Constants/Extensions.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension NSObject {
+    var className: String {
+        return String(describing: type(of: self))
+    }
+    
+    class var className: String {
+        return String(describing: self)
+    }
+}
